3636458489b51324d662f46d100055f9c92ac10c
benjaminolmsted <benjamin.olmsted@gmail.com>
2025-05-15 08:33:54 -0700

    Lobby game styling and some task list updates (#33)

--- src/games/lobby/UniqueEmoji.tsx
+++ src/games/lobby/UniqueEmoji.tsx
@@ -67,9 +67,7 @@ const GameContainer = styled(Box)(({ theme }) => ({
   position: 'relative',
   width: '100%',
   height: '100%',
-  minHeight: 800,
   overflow: 'hidden',
-  borderRadius: theme.shape.borderRadius,
   color: theme.palette.common.white,
 }));
 
